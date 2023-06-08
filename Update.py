0b10f9fba675cc5de5d57d76c9a65ffd0a22a039
MaMad <124074244+M0HMD-M@users.noreply.github.com>
2023-06-09 01:39:36 +0330

    Update Update.py

--- Update.py
+++ Update.py
@@ -1,7 +1,7 @@
 import os
 
 
-print('--------> Downloading configer.py\n\n')
+print('--------> 𝑫𝒐𝒘𝒏𝒍𝒐𝒂𝒅𝒊𝒏𝒈 𝒄𝒐𝒏𝒇𝒊𝒈𝒆𝒓 \n\n')
 os.system('curl -Lo /root/configer/configer.py https://raw.githubusercontent.com/M0HMD-M/sing-box-tg-bot-configer/master/configer.py')
 os.system('systemctl restart configer.service')
-print('Done, enjoy!')
+print(' 𝑶𝒌 𝒆 ✔ , 𝑬𝒏𝒋𝒐𝒚 𝒊𝒕 ❤ ')
